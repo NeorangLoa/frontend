806cc999fbaa01f4a24916c541ce953f5db72801
BellYun <whd3558@naver.com>
2023-05-11 23:53:28 +0900

    초기설정 수정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = { //app 경로는 아직 experimental 로 구분되기 때문에 next.config.js 파일에 아래와 같은 설정을 추가
+    experimental:{
+        appDir : true
+    }
+}
 
 module.exports = nextConfig
