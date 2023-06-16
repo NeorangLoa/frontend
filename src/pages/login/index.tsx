e0e05e0a333867a66d100558e863a666e4316029
SH <tjfgnldkdlel@naver.com>
2023-06-16 20:47:55 +0900

    로그인/회원가입 페이지 (1)

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/img/썸네일 수정.png';
 import LoginComponent from 'src/components/login';
+import SignUpComponent from 'src/components/signup';
 import './index.scss';
 
 export default function Login() {
+  const [viewLoginSignup, setViewLoginSignup] = useState(true);
+
   return (
     <div className='page'>
       <div className="left">
@@ -15,15 +18,31 @@ export default function Login() {
       </div>
 
       <div className="right">
-        <img src={logo} className='logo' />
+        {/* <img src={logo} className='logo' /> */}
         {/* <button className='sociallogin'>소셜로그인</button> */}
 
-        <LoginComponent />
+        <h1 className='title'>나랑로아</h1>
+        <div className='buttonGroup'>
+          <button
+            className='loginButton'
+            onClick={() => {setViewLoginSignup(true);}}
+          >
+            로그인
+          </button>
+          <button
+            className='signupButton'
+            onClick={() => {setViewLoginSignup(false);}}
+          >
+            회원가입
+          </button>
+        </div>
 
-        <div className='footer'>
+        { viewLoginSignup ? <LoginComponent/> : <SignUpComponent/> }
+
+        {/* <div className='footer'>
           <p>아이디가 없으신가요?</p>
           <button className='moveTosignup'>회원가입</button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
