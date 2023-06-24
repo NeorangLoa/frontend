bf2816de04b28625d6f770bf977b2892639b9339
BellYun <whd3558@naver.com>
2023-06-24 22:00:57 +0900

    feat: Nav 네비게이션

--- src/components/common/nav/nav.tsx
+++ src/components/common/nav/nav.tsx
@@ -1,16 +1,34 @@
 import React from "react";
 import './nav.scss'
+import { useNavigate } from "react-router-dom";
 
 export default function Nav(){
+
+  const navigate = useNavigate();
+
+
+  const gotomain = () =>{
+    navigate(`/`)
+  }
+  
+  const gotopost = () =>{
+    navigate(`/postlist`)
+  }
+
+  const gotologin = ()=>{
+    navigate(`login`)
+  }
+
+
     return(
       <div className="navlayout">
         <div className="space_between">
           <div className="rightdiv">
             <div className="navdiv">
-              <div className="navbutton">
+              <div className="navbutton" onClick={gotomain}>
                 나랑로아
               </div>
-              <div className="navbutton">
+              <div className="navbutton" onClick={gotopost} >
                 자유게시판
               </div>
               <div className="navbutton">
@@ -19,7 +37,7 @@ export default function Nav(){
             </div>
           </div>
           <div className="leftdiv">
-            <div className="navbutton">로그인</div>
+            <div className="navbutton" onClick={gotologin}>로그인</div>
           </div>
         </div>
       </div>
