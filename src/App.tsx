281576f94ad50e74bd393b127f26034a2764b1fa
BellYun <whd3558@naver.com>
2023-06-24 14:33:56 +0900

    feat : footer 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useRoutes } from 'react-router-dom';
 import { routes } from './Routes';
 import Nav from './components/common/nav/nav';
+import Footer from './components/common/footer/footer';
 
 function App() {
   const elem = useRoutes(routes);
   return <div>
     <Nav/>
     {elem}
+    <Footer/>
     </div>;
 }
 
