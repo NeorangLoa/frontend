2243e1b725d50db570d22e9e4e88934ec296f3dd
BellYun <whd3558@naver.com>
2023-06-24 14:34:31 +0900

    feat : 게시판 레이아웃

--- src/pages/board/free/list/index.tsx
+++ src/pages/board/free/list/index.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import './index.scss'
 
 
 export interface PostListType{
@@ -11,8 +12,6 @@ export interface PostListType{
 
 const PostList = () => {
     const [listData, setListData] = useState<PostListType[]|undefined>()
-
-
     useEffect(()=>{
         const postListData =async () => {
             try {
@@ -25,15 +24,83 @@ const PostList = () => {
             }
         }
 
-
-
         postListData()
     },[])
 
     return(
-        <div>
-            <div>
-                {listData?.map((listDatas)=><div>{listDatas.title}</div>)}
+        <div className="postlistlayout">
+            <div className="postlistmain">
+                <div className="AD">
+                    AD
+                </div>
+                <div className="boardname">
+                    자유게시판
+                </div>
+                <div className="postlistbestpost">
+                    <div className="bestpostname">
+                        <div className="bestpostnametext">
+                            추천 게시물
+                        </div>
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+                <div className="postlistnewpost">
+                번호 제목 작성자 작성일 조회 개추
+                {listData?.map((listDatas)=>
+                            <div className="bestpostlayout">
+                            <div className="postidlayout">
+                                {listDatas.postId}
+                            </div>
+                            <div className="typelayout">
+                                잠담
+                            </div>
+                            <div className="titlelayout">
+                                {listDatas.title}
+                            </div>
+                            <div className="nicknamelayout">
+                                닉네임
+                            </div>
+                            <div className="datelayout">
+                                21.21.21
+                            </div>
+                            <div className="viewcountlayout">
+                                {listDatas.viewCounts}
+                            </div>
+                        </div>
+                        )}
+                                {listData?.map((listDatas)=>
+                            <div className="bestpostlayout">
+                            <div className="postidlayout">
+                                {listDatas.postId}
+                            </div>
+                            <div className="typelayout">
+                                잠담
+                            </div>
+                            <div className="titlelayout">
+                                {listDatas.title}
+                            </div>
+                            <div className="nicknamelayout">
+                                닉네임
+                            </div>
+                            <div className="datelayout">
+                                21.21.21
+                            </div>
+                            <div className="viewcountlayout">
+                                {listDatas.viewCounts}
+                            </div>
+                        </div>
+                        )}
+                </div>
+                <div className="writebuttonlayout">
+                    <div>
+                        글쓰기
+                    </div>
+                </div>
+                <div className="pagenationlayout">
+                    페이지네이션
+                </div>
             </div>
         </div>
     )
