652c77f8bfe171cee25f231187692572bcc24d32
BellYun <whd3558@naver.com>
2023-07-19 00:04:31 +0900

    로그인 회원가입 구현

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,15 +1,36 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
 export default function LoginComponent() {
-  const [userIDInput, setUserIDInput] = useState('');
-  const [userPWInput, setUserPWInput] = useState('');
-  const [showPW, setShowPW] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const setId = (e:any) =>{
+    setEmail(e.target.value)
+    console.log(email)
+  }
+
+  const setPw = (e:any) =>{
+    setPassword(e.target.value)
+    console.log(password)
+  }
+
+  const handleLogin =async (e:any) => {
+    console.log(email);
+    console.log(password);
+    try{
+      const response =await axios.post(`http://localhost:8080/api/v1/login`,{email,password})
+      console.log(response)
+    }catch(e){
+      console.log(e);
+    }
+  }
 
   return (
     <fieldset className="loginForm">
-      <input className='ID' type='text' placeholder='아이디'></input>
-      <input className='PASSWORD' type='password' placeholder='비밀번호'></input>
-      <button className='LoginButton'>로그인</button>  {/* submit */}
+      <input className='ID' type='text' placeholder='아이디' onChange={setId}></input>
+      <input className='PASSWORD' type='password' placeholder='비밀번호' onChange={setPw}></input>
+      <button className='LoginButton' onClick={handleLogin}>로그인</button>  {/* submit */}
     </fieldset>
   );
 }
--- src/components/signup/index.tsx
+++ src/components/signup/index.tsx
@@ -1,13 +1,46 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 export default function SignUpComponent(){
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [nickname, setName] = useState('');
+
+
+  const setId = (e:any) =>{
+    setEmail(e.target.value)
+    console.log(email)
+  }
+
+  const setPw = (e:any) =>{
+    setPassword(e.target.value)
+    console.log(password)
+  }
+
+  const setNames = (e:any) =>{
+    setName(e.target.value)
+    console.log(nickname)
+  }
+
+  const handleSign =async (e:any) => {
+    console.log(email);
+    console.log(password);
+    console.log(nickname)
+    try{
+      const response =await axios.post(`http://localhost:8080/api/v1/user`,{email,password,nickname})
+      console.log(response)
+    }catch(e){
+      console.log(e);
+    }
+  }
+    
     return(
         <fieldset className="SignUpForm">
-            <input className='EMAIL' placeholder='이메일'></input>
-            <input className='ID' type='text' placeholder='아이디'></input>
-            <input className='PASSWORD' type='password' placeholder='비밀번호'></input>
+            <input className='EMAIL' placeholder='이메일'onChange={setId} value={email} ></input>
+            <input className='ID' type='text' placeholder='닉네임' onChange={setNames} value={nickname} ></input>
+            <input className='PASSWORD' type='password' placeholder='비밀번호' onChange={setPw}></input>
             <input className='rePASSWORD' type='password' placeholder='비밀번호 확인'></input>
-            <button className='SignUpButton'>회원가입</button>  {/* submit */}
+            <button className='SignUpButton' onClick={handleSign}>회원가입</button>  {/* submit */}
         </fieldset>
     )
 }
